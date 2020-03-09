947b27a4d282170afe51e6c539e8f2457a2d94a1
roman120487 <roman120487@ukr.net>
2020-03-09 20:27:51 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ContactsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
