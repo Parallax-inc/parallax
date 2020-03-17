542a90e48b52dc4714f7bc6a809e4e66dc99ca32
roman120487 <roman120487@ukr.net>
2020-03-17 20:34:45 +0200

    update

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  ImgParallax: boolean;
   constructor() { }
 
   ngOnInit() {
+   
   }
+  @HostListener('window:scroll', ['$event']) onscroll(event): void {
+    const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
+    // console.log(top);
+    if (top > 2200) {
+      this.ImgParallax = false;
+    } else {
+      this.ImgParallax = true;
+    }
+  }
+
+  
 
 }
