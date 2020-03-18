694d68699d71f16c9eeb2bd06fb2e51400303911
roman120487 <roman120487@ukr.net>
2020-03-18 13:30:56 +0200

    1

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import anime from 'animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-main',
@@ -7,10 +8,12 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   ImgParallax: boolean;
+  title = 'Веб студия - PARALLAX';
   constructor() { }
 
   ngOnInit() {
-   
+    this.animatetitle();
+
   }
   @HostListener('window:scroll', ['$event']) onscroll(event): void {
     const top = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
@@ -22,6 +25,41 @@ export class MainComponent implements OnInit {
     }
   }
 
-  
+  animatetitle() {
+    let mas = this.title.split('');
+    let maintitle = document.getElementsByClassName('mainTitle');
+    // let span = document.createElement("span");
+    let span = document.createElement("span")
+
+    console.log(mas);
+    // debugger
+    mas.forEach((elem) => {
+      setTimeout(() => {
+        // maintitle[0].appendChild(span).textContent = `${elem}`;
+        maintitle[0].insertAdjacentHTML('beforeEnd' ,`<span>${elem}</span>`);
+
+        console.log(elem);
+
+      }, 1000);
+    })
+
+
+
+  }
+
+  // anime({
+  //   targets: `${this.title}`,
+  //     // translateY: 100,
+  //     // translateY: -100,
+  //     // easing: 'linear',
+  //     duration: 10000,
+  //       // delay: 1000,
+  //       loop: true,
+  //         opacity: 1
+  //   });;
+  // }
+
+
+
 
 }
